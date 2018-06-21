fdade3e51e4a909746c8fbb3c8b93e26b8f4f731
Patryk Jan Matlak <patrykjan.matlak@gmail.com>
2018-06-21 10:15:33 +0200

    Adding simple templates with variables

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,21 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
+var_context="Mango"
 # Create your views here.
 def home(request):
-    html_="""<!-- A trilogy of books with numbered volumes. -->
-<div>
-  <p>
-    The <strong>Lord of the Rings</strong> is an English-language fictional trilogy by J. R. R. Tolkien (1892-1973).
-  </p>
-  <p>
-    The books in the trilogy are:
-  </p>
-  <ul>
-    <li>Vol. 1: The Fellowship of the Ring</li>
-    <li>Vol. 2: The Two Towers</li>
-    <li>Vol. 3: The Return of the King</li>
-  </ul>
-</div> """
-    return HttpResponse(html_)
-    #return render(request, "home.html") 
+  return render(request, "base.html", {"var1":var_context}) 
