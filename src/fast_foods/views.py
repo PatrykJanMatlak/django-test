12aa49e4c5ecaea954b7f03d18edaecc38c3a02e
Patryk Jan Matlak <patrykjan.matlak@gmail.com>
2018-06-23 18:17:26 +0200

    Adding fast foods data showing

--- src/fast_foods/views.py
+++ src/fast_foods/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from .models import FastFoodScore
 
 # Create your views here.
+
+
+def ff_listview(request):
+  template_name="fast_foods/ff_list.html"
+  
+  queryset2 = FastFoodScore.objects.all()
+
+  context={
+    "list2":queryset2
+
+  }
+  return render(request, template_name ,context)
--- src/projekt/urls.py
+++ src/projekt/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from restaurants.views import restaurant_listview
+from fast_foods.views import ff_listview
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name="home.html")),
     url(r'^restaurants/', restaurant_listview),
+    url(r'^fast_foods/', ff_listview),
     url(r'^contact/',TemplateView.as_view(template_name="contact.html")),
     url(r'^about/', TemplateView.as_view(template_name="about.html"))
 ]
