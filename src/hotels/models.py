44577699c6085ca52ac1a907a2deff68427d1f51
Patryk Jan Matlak <patrykjan.matlak@gmail.com>
2018-06-26 12:08:26 +0200

    changes to hotel's model

--- src/hotels/models.py
+++ src/hotels/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class HotelOpinion(models.Model):
     name = models.CharField(max_length=120, null = False, blank = False)
-    location = models.CharField(max_length=120)
+    city = models.CharField(max_length=120, null = True, blank = True)
+    street = models.CharField(max_length=120, null = True, blank = True)
+    stars_number = models.IntegerField(verbose_name="Number of the stars:", null=True, blank = True)
+    opinion =  models.TextField(max_length=400 , null = False, blank = False)
 
     def __str__(self):
         return self.name
