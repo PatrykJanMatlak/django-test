1164ff39f02fa2522d9cf5db8101b0b1fbfd86ab
Patryk Jan Matlak <patrykjan.matlak@gmail.com>
2018-07-04 18:13:43 +0200

    User connecting with class-based view

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -8,26 +8,6 @@ from .forms import  RestaurantLocationCreateForm
 from django.http import HttpResponseRedirect
 
 
-def restaurant_createvie(request):
-  template_name = 'restaurants/dform.html'
-  form = RestaurantLocationCreateForm(request.POST or None)
-  errors = None
-  
-  if form.is_valid():
-    form.save()
-    return HttpResponseRedirect("/restaurants/")
-
-  if form.errors:
-    print (form.errors)
-    errors = form.errors
-    
-  context = {"form":form, "errors":errors}
-
-  
-  
-  return render(request, template_name, context)
-
-
 class RestaurantListView(ListView):
   queryset = RestaurantLocation.objects.all()
   def get_queryset(self):
@@ -50,27 +30,32 @@ class RestaurantCreateView(CreateView):
 
   success_url="/restaurants/"
 
-
-
-def restaurant_createview(request):
-
-  template_name = 'restaurants/dform.html'
-  form = RestaurantLocationCreateForm(request.POST or None)
-  errors = None
-  
-  if form.is_valid():
-    if request.user.is_authenticated():
-      instance = form.save(commit = False)
-      instance.owner = request.user
-      instance.save()
-      return HttpResponseRedirect("/restaurants/")
-    else:
-      return HttpResponseRedirect("/about/")
-
-  if form.errors:
-    print (form.errors)
-    errors = form.errors
+  def form_valid(self, form):
+    instance = form.save(commit = False)
+    instance.owner = self.request.user
+    #instance.save()
+    return super(RestaurantCreateView, self).form_valid(form)
     
-  context = {"form":form, "errors":errors}
 
-  return render(request, template_name, context)
+# def restaurant_createview(request):
+
+#   template_name = 'restaurants/dform.html'
+#   form = RestaurantLocationCreateForm(request.POST or None)
+#   errors = None
+  
+#   if form.is_valid():
+#     if request.user.is_authenticated():
+#       instance = form.save(commit = False)
+#       instance.owner = request.user
+#       instance.save()
+#       return HttpResponseRedirect("/restaurants/")
+#     else:
+#       return HttpResponseRedirect("/about/")
+
+#   if form.errors:
+#     print (form.errors)
+#     errors = form.errors
+    
+#   context = {"form":form, "errors":errors}
+
+#   return render(request, template_name, context)
